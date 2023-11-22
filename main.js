64165cb9355424c253a27b4c4bb2e7ccd33d3804
bingisainath <84036820+bingisainath@users.noreply.github.com>
2023-11-22 18:34:48 +0530

    section-1

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
-const {app,BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain, Notification } = require("electron");
+const path = require("path");
+const isDev = !app.isPackaged;
 
-function createWindow(){
-    const win = new BrowserWindow({
-        width:800,
-        height:800,
-        webPreferences:{
-            nodeIntegration:true
-        },
-        backgroundColor:'white'
-    })
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    backgroundColor: "white",
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      // worldSafeExecuteJavaScript:true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-    win.loadFile('index.html');
+  isDev && win.webContents.openDevTools();
+  win.loadFile("index.html");
+}
+
+if (isDev) {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+  });
 }
 
 app.whenReady().then(createWindow);
 
-app.on("window-all-closed",() => {
-    if(process.platform !== 'darwin'){
-        app.quit();
-    } 
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
 
-app.on('activate',() =>{
-    if(BrowserWindow.getAllWindows.length === 0){
-        createWindow();
-    }
-})
+ipcMain.on("notify", (e, message) => {
+  new Notification({ title: "chat app", body: message }).show();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows.length === 0) {
+    createWindow();
+  }
+});
